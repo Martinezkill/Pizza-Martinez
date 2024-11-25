94cb6945757592af9ce1781d04b9f1e5b20a4af2
Emanuel Martinez <martinezemanuel729@gmail.com>
2024-11-25 22:26:49 +0000

    Construction of the login page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,27 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
-    return (
-        <div>
-            <h1>Autenticacao</h1>
-
-            <div>
-                <Outlet/>
-            </div>
+  return (
+    <div className="grid min-h-screen grid-cols-2">
+      {/* Coluna esquerda */}
+      <div className="flex flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        {/* Cabeçalho */}
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza martinez</span>
         </div>
-    )
+
+        {/* Rodapé */}
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.martinez - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      {/* Coluna direita */}
+      <div className="bg-white flex items-center justify-center">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
