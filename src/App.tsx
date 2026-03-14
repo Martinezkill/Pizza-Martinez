b76c8be0755d5d51b5c944b38e6fb06aaba746c8
Emanuel Martinez <131058355+Martinezkill@users.noreply.github.com>
2026-03-14 15:16:47 +0000

    details update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
